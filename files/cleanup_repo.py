9ff4b1a3d5d5a590e17dcb843c6cf787f12723b4
Cedric LECOMTE <clecomte@redhat.com>
2018-07-24 10:13:10 +0200

    Optimize the component download by reposync only when it's necessary
    Add also a cleaner to avoid problem when package is upgraded without version change
    
    Change-Id: I6a8726e9d3019ae4eb15f5a5144fe9fa51fa9eb9

---
+++ files/cleanup_repo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from lxml import etree
+import os
+import hashlib
+import StringIO
+
+def sha256_checksum(filename, block_size=65536):
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(block_size), b''):
+            sha256.update(block)
+        return sha256.hexdigest()
+
+tree = etree.parse("/tmp/test.xml")
+root = tree.getroot()
+print(root)
+for package in root:
+    print(package[10].attrib['href'])
+    if(os.path.isfile("/repo/RHEL/" + package[10].attrib['href'])):
+	found_sha256 = sha256_checksum("/repo/RHEL/" + package[10].attrib['href'])
+	if (found_sha256 == package[3].text):
+	    print("OK");
+        else:
+            print("Delete : %s" % ("/repo/RHEL/" + package[10].attrib['href']))
+            os.remove("/repo/RHEL/" + package[10].attrib['href'])
