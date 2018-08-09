6a95c0a6221ff44f5f6f6b44c88147f6800189df
Dimitri Savineau <dsavinea@redhat.com>
2018-08-09 16:45:26 -0400

    cleanup_repo.py: Use product name as parameter
    The RHEL product name was hardcoded and it was not possible to use
    the cleanup script with another product (ex: OpenStack).
    This patch adds an arg to the script wich is the product name.
    
    Change-Id: If8255f9962cdc0d7f20c148db78c2695dd499100

--- files/cleanup_repo.py
+++ files/cleanup_repo.py
@@ -2,6 +2,7 @@
 
 from lxml import etree
 import os
+import sys
 import hashlib
 import StringIO
 
@@ -12,15 +13,20 @@ def sha256_checksum(filename, block_size=65536):
             sha256.update(block)
         return sha256.hexdigest()
 
+if len(sys.argv) != 2:
+    print("Product name is required")
+    sys.exit(1)
+
+product = sys.argv[1]
 tree = etree.parse("/tmp/test.xml")
 root = tree.getroot()
 print(root)
 for package in root:
     print(package[10].attrib['href'])
-    if(os.path.isfile("/repo/RHEL/" + package[10].attrib['href'])):
-	found_sha256 = sha256_checksum("/repo/RHEL/" + package[10].attrib['href'])
+    if(os.path.isfile("/repo/" + product + "/" + package[10].attrib['href'])):
+	found_sha256 = sha256_checksum("/repo/" + product + "/" + package[10].attrib['href'])
 	if (found_sha256 == package[3].text):
 	    print("OK");
         else:
-            print("Delete : %s" % ("/repo/RHEL/" + package[10].attrib['href']))
-            os.remove("/repo/RHEL/" + package[10].attrib['href'])
+            print("Delete : %s" % ("/repo/" + product + "/" + package[10].attrib['href']))
+            os.remove("/repo/" + product + "/" + package[10].attrib['href'])
