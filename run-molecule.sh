c12d4c3b4f5a515d8892dffeca56ded40023b77a
Haikel Guemar <hguemar@fedoraproject.org>
2020-06-18 13:25:45 +0200

    DNM: dummy change
    Change-Id: If329ae7bad79e6bb5ff2449047c1fb3bb9278144

---
+++ run-molecule.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -x
+
+source /etc/os-release
+
+cat /etc/os-release
+
+DRIVER_ARGS=""
+
+
+if [[ $DISTRO == "centos" ]] && (( $VERSION_ID > 7)); then
+    DRIVER_ARGS="--driver=podman"
+    echo "Using podman driver"
+fi
+
+molecule test ${DRIVER_ARGS}
